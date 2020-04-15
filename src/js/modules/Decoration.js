3e097e8f523a24dc8aa600d6336b4effe9e78e1f
Sebastian <monika.lukowska@gmail.com>
2020-04-15 21:56:52 +0200

    improved hero animation: js add class animated on scroll; adapt scss styles; add transform:translate  on hover

--- src/js/modules/Decoration.js
+++ src/js/modules/Decoration.js
@@ -1,31 +1,28 @@
 class Decoration{
     constructor(){
-        this.right = document.querySelector('.decoration--right');
-        this.rightTop = document.querySelector('.decoration--right--t');
-        this.rightBottom = document.querySelector('.decoration--right--b');
-        this.leftTop = document.querySelector('.decoration--left--t');
-        this.leftBottom = document.querySelector('.decoration--left--b');
-        this.event();
+        this.decorationRight = [...document.querySelector('.decoration--right').children];
+        this.decorationLeft = [...document.querySelector('.decoration--left').children];
+        this.event();    
     }
 
     event(){
-        window.addEventListener('scroll', () => this.moveStripes());
+        window.addEventListener('scroll', () => this.addClassAnimation())
     }
 
-    moveStripes(){
+    addClassAnimation(){
         let lastScrollTop = 100
         let st = window.pageYOffset || document.documentElement.scrollTop;
-        if(st > lastScrollTop){
-            this.rightTop.style.left = "-50%";
-            this.rightBottom.style.left = "-50%"; 
-            this.leftTop.style.left = "-50%"; 
-            this.leftBottom.style.left = "-50%"; 
-        } else {
-            this.rightTop.style.left = "20%";
-            this.rightBottom.style.left = "20%";
-            this.leftTop.style.left = "20%";
-            this.leftBottom.style.left = "20%";
-        }
+         if(st > lastScrollTop){
+             for(let i = 0; i < this.decorationLeft.length; i++){
+                 this.decorationLeft[i].classList.add('animated');
+                 this.decorationRight[i].classList.add('animated');
+             }
+         }else{
+              for(let i = 0; i < this.decorationLeft.length; i++){
+                 this.decorationLeft[i].classList.remove('animated');
+                 this.decorationRight[i].classList.remove('animated');
+             }
+         }
     }
 
 }
